315fd1cab6d26a0e14f3af19ee7571a361b154cb
Isabeau <isabeaujax@gmail.com>
2023-10-16 22:42:59 -0400

    Fixed Howdy Doody section layout and made the references section on tablet look like mobile Co-authored-by: Kyle Vasulka <KyleJVasulka@gmail.com>

--- app/howdyDoodie.tsx
+++ app/howdyDoodie.tsx
@@ -6,20 +6,21 @@ import { useState, useEffect } from 'react';
 
 
 function HowdyDoodie(props: any) {
-    return (<div className={`${props.className} ${HowdyDoodieCSS.horizFlex}`}>
-        <div className={`${HowdyDoodieCSS.picSide}`}>
-            <div className={`${HowdyDoodieCSS.image}`}>
-                <img src='/howdy2.png' className={`${HowdyDoodieCSS.image2}`} alt="SVG Display" />
-                <img src='/rays.svg' className={`${HowdyDoodieCSS.rays}`} alt="SVG Display" />
+    return (
+        <div className={`${props.className} ${HowdyDoodieCSS.flex}`}>
+            <div className={`${HowdyDoodieCSS.picSide}`}>
+                <div className={`${HowdyDoodieCSS.image}`}>
+                    <img src='/rays.svg' className={`${HowdyDoodieCSS.rays}`} alt="SVG Display" />
+                    <img src='/howdy2.png' className={`${HowdyDoodieCSS.image2}`} alt="SVG Display" />
+                </div>
+            </div>
+            <div className={`${HowdyDoodieCSS.textSide}`}>
+                <h1 className={`${HowdyDoodieCSS.text}`}>Howdy Doodie, Pardner</h1>
+                <p className={`${HowdyDoodieCSS.quoteText}`}>I&apos;m Isabeau Jackson, a passionate UI/UX designer proudly rooted near Cleveland, Ohio. I specialize in blending artistry and empathy to orchestrate intuitive digital experiences.</p>
+                <p className={`${HowdyDoodieCSS.quoteText}`}>I may be a dedicated UI/UX designer, but I have a fervent love for music that runs just as deep. While I thrive in the world of pixels and user experiences, my heart beats to the rhythm of melodies and harmonies while I listen to/play/write music.</p>
             </div>
         </div>
-        <div className={`${HowdyDoodieCSS.textSide}`}>
-            <h1 className={`${HowdyDoodieCSS.text}`}>Howdy Doodie, Pardner</h1>
-            <p className={`${HowdyDoodieCSS.quoteText}`}>I&apos;m Isabeau Jackson, a passionate UI/UX designer proudly rooted near Cleveland, Ohio. I specialize in blending artistry and empathy to orchestrate intuitive digital experiences.</p>
-            <p className={`${HowdyDoodieCSS.quoteText}`}>I may be a dedicated UI/UX designer, but I have a fervent love for music that runs just as deep. While I thrive in the world of pixels and user experiences, my heart beats to the rhythm of melodies and harmonies while I listen to/play/write music.</p>
-        </div>
-
-    </div>)
+    )
 }
 
 export default HowdyDoodie
